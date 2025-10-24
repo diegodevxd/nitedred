6913af1696be6ee943384e68569dad2fdbf7b272
diegodevxd <diegomancera.dev@gmail.com>
2025-10-24 11:39:33 -0600

    Add files via upload

---
+++ src/src/js/global-functions.js
@@ -0,0 +1,42 @@
+// Global Functions Wrapper
+// This file exposes placeholder functions immediately so onclick handlers don't fail
+// The real implementations will be set by app.js when it loads
+
+window.loginWithWallet = function() { console.log('loginWithWallet loading...'); };
+window.loginWithGoogle = function() { console.log('loginWithGoogle loading...'); };
+window.showRegister = function() { console.log('showRegister loading...'); };
+window.showLogin = function() { console.log('showLogin loading...'); };
+window.register = function() { console.log('register loading...'); };
+window.logout = function() { console.log('logout loading...'); };
+window.showSection = function() { console.log('showSection loading...'); };
+window.showCreateHomePost = function() { console.log('showCreateHomePost loading...'); };
+window.hideCreateHomePost = function() { console.log('hideCreateHomePost loading...'); };
+window.addMedia = function() { console.log('addMedia loading...'); };
+window.createHomePost = function() { console.log('createHomePost loading...'); };
+window.toggleLike = function() { console.log('toggleLike loading...'); };
+window.toggleComments = function() { console.log('toggleComments loading...'); };
+window.addComment = function() { console.log('addComment loading...'); };
+window.showToast = function() { console.log('showToast loading...'); };
+window.removeMedia = function() { console.log('removeMedia loading...'); };
+window.openCloudinaryUpload = function() { console.log('openCloudinaryUpload loading...'); };
+window.getCurrentMedia = function() { console.log('getCurrentMedia loading...'); return null; };
+window.clearCurrentMedia = function() { console.log('clearCurrentMedia loading...'); };
+window.loadSavedPosts = function() { console.log('loadSavedPosts loading...'); };
+window.loadSavedStories = function() { console.log('loadSavedStories loading...'); };
+window.openStoryUpload = function() { console.log('openStoryUpload loading...'); };
+window.loadCryptoNews = function() { console.log('loadCryptoNews loading...'); };
+window.openNewsArticle = function() { console.log('openNewsArticle loading...'); };
+window.showCryptoChart = function() { console.log('showCryptoChart loading...'); };
+window.hideCryptoChart = function() { console.log('hideCryptoChart loading...'); };
+window.toggleFollow = function() { console.log('toggleFollow loading...'); };
+window.isFollowing = function() { console.log('isFollowing loading...'); return false; };
+window.showNewChat = function() { console.log('showNewChat loading...'); };
+window.hideNewChat = function() { console.log('hideNewChat loading...'); };
+window.showChatTab = function() { console.log('showChatTab loading...'); };
+window.loadChats = function() { console.log('loadChats loading...'); };
+window.openChat = function() { console.log('openChat loading...'); };
+window.backToChats = function() { console.log('backToChats loading...'); };
+window.sendMessage = function() { console.log('sendMessage loading...'); };
+window.showChatOptions = function() { console.log('showChatOptions loading...'); };
+window.archiveCurrentChat = function() { console.log('archiveCurrentChat loading...'); };
+window.blockUser = function() { console.log('blockUser loading...'); };
